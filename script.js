004e3fd36c27ec408223fc0d6ab570cacd4e4496
Leonardo Pernomian <leonardopernomian@hotmail.com>
2024-07-10 10:13:19 -0300

    finalizando o projeto :)

--- script.js
+++ script.js
@@ -15,16 +15,3 @@ ShowText2.addEventListener('click', () => {
 
 
 
-// document.querySelectorAll('h2').forEach(title => {
-//     title.addEventListener('click', () => {
-//         const nextElement = title.nextElementSibling;
-//         if (nextElement) {
-//             nextElement.classList.toggle('hidden');
-//             if (nextElement.classList.contains('hidden')) {
-//                 nextElement.style.maxHeight = '0';
-//             } else {
-//                 nextElement.style.maxHeight = nextElement.scrollHeight + 'px';
-//             }
-//         }
-//     });
-// })
